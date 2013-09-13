a48dbedeb4a50cd3713e16e70ea3fc16c1c6ff51
Ashvin Nair <nair.ashvin@gmail.com>
2013-09-12 19:44:11 -0700

    Added parsing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-f = open('cube.txt', 'r')
-lines = []
-for line in f:
-    lines.append(line.split(","))
-print lines
+
+def parse(filename = "cube.txt"):
+    f = open(filename, 'r')
+    lines = []
+    for line in f:
+        line = line.strip().split(",")
+        lines.append([l.strip() for l in line])
+    return lines
+
+print parse()
