bdc4e5ce10e8a1919ddc7fc48ce00aa2a4cae1f3
Deepak Kumar <deepak9308@gmail.com>
2013-09-12 19:49:05 -0700

    Added Direction, Move, and Face classes for abstraction.

--- main.py
+++ main.py
@@ -3,3 +3,36 @@ lines = []
 for line in f:
     lines.append(line.split(","))
 print lines
+
+class Directions:
+    """
+    Class to represent directions that each face can move.
+    """
+    CLOCKWISE = 'Clockwise'
+    COUNTERCLOCKWISE = 'Counterclockwise'
+
+class Face:
+    """
+    Class to represent each face on rubix cube.
+    """
+    TOP = 'Top'
+    LEFT = 'Left'
+    FRONT = 'Front'
+    RIGHT = 'RIGHT'
+    BACK = 'Back'
+    BOTTOM = 'Bottom'
+
+class Move:
+    """
+    Class that represents a move on the rubix cube.
+    Attributes include direction (clockwise, counterclockwise) and a face (top, left, front, right, back, bottom)
+    """
+    def __init__(self, direction, face):
+        self.direction = direction
+        self.face = face
+    
+    def getDirection(self):
+        return self.direction
+    
+    def getFace(self):
+        return self.face
