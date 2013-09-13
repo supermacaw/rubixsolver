1e7e35acbc5b9f9d727819f76700f49854f0d72b
Ashvin Nair <nair.ashvin@gmail.com>
2013-09-12 21:01:33 -0700

    ADSGADF

--- move.py
+++ move.py
@@ -21,12 +21,20 @@ def up_cw(cube):
     return cube
     
 def left_cw(cube):
+    rotate_face_cw(cube[1])
+    rotate_faces_cw(cube, [0, 2, 5, 4], [[1, 4, 7], [1, 4, 7], [1, 4, 7], [8, 5, 2]])
+
+def left_ccw(cube):
     rotate_face_ccw(cube[1])
-    m = cube[1][:3]
-    cube[1][:3] = cube[2][:3]
-    cube[2][:3] = cube[3][:3]
-    cube[3][:3] = cube[4][:3]
-    cube[4][:3] = m
+    rotate_faces_ccw(cube, [0, 2, 5, 4], [[1, 4, 7], [1, 4, 7], [1, 4, 7], [7, 4, 1]])    
+
+def front_cw(cube):
+    rotate_faces_cw(cube, [0, 3, 5, 1], [[6, 7, 8], [0, 3, 6], [0, 1, 2], [2, 5, 8]])
+    rotate_face_cw(cube[2])
+
+def front_ccw(cube):
+    rotate_faces_ccw(cube, [0, 3, 5, 1], [[6, 7, 8], [0, 3, 6], [0, 1, 2], [2, 5, 8]])
+    rotate_face_ccw(cube[2])
 
 def rotate_face_ccw(face):
     face[0] = face[2]
@@ -49,14 +57,35 @@ def rotate_face_cw(face):
     face[0] = face[6]
     face[3] = face[7]
     face[6] = face[8]
+    
+def rotate_faces_ccw(cube, faces, faces_squares):
+    return rotate_faces(cube, faces[::-1], faces_squares[::-1])
+    
+def rotate_faces_cw(cube, faces, faces_squares):
+    return rotate_faces(cube, faces, faces_squares)
+    
+def rotate_faces(cube, faces, faces_squares):
+    temp = [cube[faces[0]][i] for i in faces_squares[0]]
+    for i in range(3):
+        for j in [3, 2, 1]:
+            cube[faces[(j + 1) % 4]][faces_squares[(j + 1) % 4][i]] = cube[faces[j]][faces_squares[j][i]]
+    for i in range(3):
+        cube[faces[1]][faces_squares[1][i]] = temp[i]
+    return cube
 
-def rotate_into(from_face, from_squares, to_face, to_squares):
-    triple = (face2[column], face2[column + 3], face2[column + 6])
-    face2[0] = face1[0]
-    face2[1] = face1[1]
-    face2[2] = face1[2]
+def rotate_into(cube, from_face, from_squares, to_face, to_squares):
+    temp = [cube[to_face][i] for i in to_squares]
+    end_squares = []
+    for i in range(3):
+        cube[to_face][to_squares[i]] = cube[from_face][from_squares[i]]
+        cube[from_face][from_squares[i]] = temp[i]
+    return cube, 
 
 cube = up_ccw(cube)
 print(cube)
 cube = up_cw(cube)
+print(cube)
+
+rotate_faces_cw(cube, [0, 3, 5, 1], [[6, 7, 8], [0, 3, 6], [0, 1, 2], [2, 5, 8]])
+rotate_faces_ccw(cube, [0, 3, 5, 1], [[6, 7, 8], [0, 3, 6], [0, 1, 2], [2, 5, 8]])
 print(cube)
